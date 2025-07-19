8e846f82ac322d45cbbad359bf9fb76235283791
pavithraumesh <pavithrababu26@gmail.com>
2025-07-19 18:45:27 +0530

    Add files via upload
    Day1

---
+++ day1/Chrome.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class Chrome {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//float
+		float val =91.0f;
+		System.out.println(val);
+		
+        //string
+		String name ="Google";
+		System.out.println(name);
+		
+		//boolean - true or false
+		boolean isBeta = false;
+		System.out.println(isBeta);
+		
+		//int
+		int releaseYear = 2008;
+		System.out.println(releaseYear);
+		
+		//char
+		char shortcutKey = 'C';
+		System.out.println(shortcutKey);
+				
+	}
+
+}
